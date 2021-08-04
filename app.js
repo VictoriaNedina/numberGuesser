0aa697c7ee9c58a553913699897462e5df3e4026
Victoria Nedina Nunes <victorianedina@ibm.com>
2021-08-04 15:10:56 -0300

    Add lose case and game over

--- app.js
+++ app.js
@@ -27,17 +27,45 @@ guessBtn.addEventListener('click', function () {
 
     // Check if won
     if (guess === winningNum) {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} está correto!`, 'green');
+        // Game over - won
+        gameOver(true, `${winningNum} está correto!`);
 
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if (guessesLeft === 0) {
+            // Game over - lost
+            gameOver(false, `Game Over. O número correto era: ${winningNum}.`);
+        } else {
+            // Game continues - answer wrong
+
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            // Clear Input
+            guessInput.value = '';
+
+            // Tell user its the wrong number
+            setMessage(`${guess} não é o número correto. Você ainda tem ${guessesLeft} chance(s) de acertar!`, 'red');
+        }
     }
 });
+
+// Game over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set message
+    setMessage(msg);
+}
 
 // Set message
 function setMessage(msg, color) {
